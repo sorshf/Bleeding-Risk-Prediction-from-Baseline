8e90928ccf578eed1f59b3cee1ebb804377f298f
Soroush Shahryari Fard <soroush.shahryarifard@gmail.com>
2023-09-21 13:57:19 -0400

    Changed the name of the features to reflect the changes made in their names in the xlsx file.

--- clinical_score.py
+++ clinical_score.py
@@ -45,18 +45,18 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
         creatinine = X['Creatinine (umol/L)'] * 0.0010
         hemoglobin = X['Hemoglobin (g/L)'] * (-0.0122)
         age = X['Age Baseline'] * 0.0256
-        antiplatelate = (1 if X['Antipiatelet agent']==1 else 0) * 0.8786
+        antiplatelate = (1 if X['Antiplatelet Agent']==1 else 0) * 0.8786
         
         return 0.0195*np.exp(creatinine+hemoglobin+age+antiplatelate)
     
     def get_ACCP_Score(self, X):
         age = 1 if 75>X['Age Baseline']>65 else (2 if X['Age Baseline']>=75 else 0)
         
-        previous_gas_bleed = 1 if X['Prior gastrointestinal bleed']==1 else 0
+        previous_gas_bleed = 1 if X['Prior Gastrointestinal Bleed']==1 else 0
         
         renal_failure = 1 if X['Creatinine (umol/L)']>106.1 else 0
         
-        previous_stroke = 1 if X['Prior stroke (CVA)']==1 else 0
+        previous_stroke = 1 if X['Prior Stroke (CVA)']==1 else 0
         
         diabete = 1 if X['Diabetes Mellitus']==1 else 0
         
@@ -65,11 +65,11 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
         else:
             anemia = 0
             
-        antiplatelate = 1 if X['Antipiatelet agent']==1 else 0
+        antiplatelate = 1 if X['Antiplatelet Agent']==1 else 0
         
-        nseid = 1 if X['Non-steroidal anti-Inflammatory (NSAIDs)']==1 else 0
+        nseid = 1 if X['NSAIDs']==1 else 0
         
-        TTR = 1 if X['INR result'] > 3 else 0 #Note this is not in the actual calculator
+        TTR = 1 if X['INR Result'] > 3 else 0 #Note this is not in the actual calculator
         
         return age+previous_gas_bleed+renal_failure+previous_stroke+diabete+anemia+antiplatelate+nseid+TTR
     
@@ -85,7 +85,7 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
             anemia = 0
         
         
-        if (X['Pulmonary embolism only']==1) | (X['Deep vein thrombosis and pulmonary embolism']==1):
+        if (X['PE only']==1) | (X['DVT and PE']==1):
             Symptomatic_pulmonary_embolism = 1
         else:
             Symptomatic_pulmonary_embolism = 0
@@ -96,7 +96,7 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
         
         age = 1.5 if X['Age Baseline']>=60 else 0
         
-        previous_gas_bleed = 1.5 if X['Prior gastrointestinal bleed']==1 else 0
+        previous_gas_bleed = 1.5 if X['Prior Gastrointestinal Bleed']==1 else 0
         
         renal_failure = 1.5 if X['Creatinine (umol/L)']>106.1 else 0
         
@@ -105,7 +105,7 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
         else:
             anemia = 0
         
-        hypertension_med = 1 if ((X['Current Treatment for hypertension']==1) & (X['Gender'] == 0)) else 0
+        hypertension_med = 1 if ((X['Current Treatment for Hypertension']==1) & (X['Gender'] == 0)) else 0
         
         return age+previous_gas_bleed+renal_failure+anemia+hypertension_med
     
@@ -113,25 +113,25 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
     def get_HAS_BLED_Score(self, X):
         age = 1 if X['Age Baseline']>65 else 0
         
-        previous_gas_bleed = 1 if X['Prior gastrointestinal bleed']==1 else 0
+        previous_gas_bleed = 1 if X['Prior Gastrointestinal Bleed']==1 else 0
         
         renal_failure = 1 if X['Creatinine (umol/L)']>200 else 0
         
-        previous_stroke = 1 if X['Prior stroke (CVA)']==1 else 0
+        previous_stroke = 1 if X['Prior Stroke (CVA)']==1 else 0
         
-        antiplatelate = 1 if X['Antipiatelet agent']==1 else 0
+        antiplatelate = 1 if X['Antiplatelet Agent']==1 else 0
         
-        TTR = 1 if X['INR result'] > 3 else 0 #Note this is not in the actual calculator
+        TTR = 1 if X['INR Result'] > 3 else 0 #Note this is not in the actual calculator
 
-        hypertension_med = 1 if X['Current Treatment for hypertension']==1 else 0
+        hypertension_med = 1 if X['Current Treatment for Hypertension']==1 else 0
         
         return age+previous_gas_bleed+renal_failure+previous_stroke+antiplatelate+TTR+hypertension_med
     
     
     def get_OBRI_Score(self, X):
         age = 1 if X['Age Baseline']>=65 else 0 #On OBRI paper, it is >= 65
-        previous_stroke = 1 if X['Prior stroke (CVA)']==1 else 0
-        previous_gas_bleed = 1 if X['Prior gastrointestinal bleed']==1 else 0
+        previous_stroke = 1 if X['Prior Stroke (CVA)']==1 else 0
+        previous_gas_bleed = 1 if X['Prior Gastrointestinal Bleed']==1 else 0
         
         renal_failure = 1 if X['Creatinine (umol/L)']>133 else 0
         
@@ -142,7 +142,7 @@ class ClinicalScore(BaseEstimator, ClassifierMixin):
         else:
             anemia = 0
         
-        myocardial_infarction = 1 if X['Prior myocardial infarction']==1 else 0
+        myocardial_infarction = 1 if X['Prior Myocardial Infarction']==1 else 0
         
         comorbidity = 1 if sum([renal_failure,diabete,anemia,myocardial_infarction])>0 else 0
         
